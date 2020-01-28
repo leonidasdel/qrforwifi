e280256469ba08e1d952b1be29dd64ba943759a1
unknown <delliolan1999@gmail.com>
2020-01-28 17:14:07 +0200

    push to github

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,59 @@
-import { Component } from '@angular/core';
-
+import { Component, Input } from '@angular/core';
+import QRCode from "qrcode"
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'qrcodewifi';
+  
+  
+  ssid:String;
+  password:String;
+  encryption:String="WPA";
+  options={width:400}
+  revealpassword(){
+    var password = (<HTMLInputElement>document.querySelector(".pwd"));
+    if(password.type==="password"){
+      password.type="text"
+    }
+    else{
+      password.type="password"
+    }
+    var eye= document.getElementById('eye')
+    console.log(eye.classList[1])
+    if(eye.classList[1]=='fa-eye-slash'){
+      eye.className='fa fa-eye'
+    }
+    else{
+      eye.className='fa fa-eye-slash'
+    }
+  }
+  addssid($event){
+    this.ssid=$event.target.value
+  }
+  addpassword($event){
+    this.password=$event.target.value
+  }
+  addencryption($event){
+    this.encryption=$event.target.value
+  }
+  preventRefresh($event){
+$event.preventDefault();
+     QRCode.toCanvas(document.getElementById('canvas'),  "WIFI:"+ "T:" + this.encryption + ";"+ "S:" + this.ssid + ";" + "P:" + this.password +";" + "H:" + "true" + ";" + ";",this.options, function (error) {
+   if (error) console.error(error)
+    console.log('success!');
+  }) 
+  }
+  scrollpx($event){
+    // window.scrollBy({ 
+    //   top: 150, // negative value acceptable
+    //   left: 0, 
+    //   behavior: 'smooth' 
+    // });
+    $event.preventDefault()
+    document.querySelector('#buttonhelper').scrollIntoView({ 
+      behavior: 'smooth' 
+    });
+  }
 }
